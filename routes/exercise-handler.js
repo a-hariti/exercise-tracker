1cc0c33cbb9bd7cc30144829945a367e787bfba4
abdellah hariti <haritiabdellah@gmail.com>
2018-10-05 15:02:43 +0100

    change default exercises limit to an arbitrary 10000

--- routes/exercise-handler.js
+++ routes/exercise-handler.js
@@ -1,31 +1,31 @@
-const Exercise = require("../models/exercise-model");
-const User = require("../models/user-model");
-const handleErrors = require("../utils/handle-errors");
+const Exercise = require('../models/exercise-model')
+const User = require('../models/user-model')
+const handleErrors = require('../utils/handle-errors')
 const getExerciseInfo = ({ description, duration, date }) => ({
   description,
   duration,
   date
-});
+})
 
 const addExercise = async (req, res) => {
   const newExercise = await Exercise.create({
     ...req.body,
     date: req.body.date ? req.body.date : new Date()
-  });
+  })
 
-  const { username, _id } = await User.findById(newExercise.userId).exec();
+  const { username, _id } = await User.findById(newExercise.userId).exec()
 
   res.json({
     username,
     _id,
     exercise: getExerciseInfo(newExercise)
-  });
-};
+  })
+}
 const logExercises = async (
-  { query: { userId, from, to, limit = Infinity } },
+  { query: { userId, from, to, limit = 10000 } },
   res
 ) => {
-  const { username, _id } = await User.findById(userId);
+  const { username, _id } = await User.findById(userId)
   const exercises = await Exercise.find({
     userId,
     ...((from || to) && {
@@ -34,16 +34,16 @@ const logExercises = async (
         ...(to && { $lt: new Date(to) })
       }
     })
-  }).limit(+limit);
+  }).limit(+limit)
   res.json({
     username,
     _id,
     count: exercises.length,
     log: exercises.map(getExerciseInfo)
-  });
-};
+  })
+}
 
 module.exports = {
   addExercise: handleErrors(addExercise),
   logExercises: handleErrors(logExercises)
-};
+}
